5b9ec3b43310af0ee2267de2d9fd5de1ef372c6e
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-02-04 17:04:11 -0800

    Rewrite deploy script

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,31 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import rehypeHighlight from 'rehype-highlight';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = nextMDX({
