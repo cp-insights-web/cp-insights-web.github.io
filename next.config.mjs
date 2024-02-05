f98f52451206fef31226de6f7f08f9615ef14c8d
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-02-04 17:09:57 -0800

    Inline next config

--- next.config.mjs
+++ next.config.mjs
@@ -12,22 +12,7 @@ const nextConfig = {
    */
   output: "export",
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+  pageExtensions: ['js', 'jsx', 'tsx', 'ts', 'md', 'mdx'],
 };
 
 const withMDX = nextMDX({
@@ -38,7 +23,4 @@ const withMDX = nextMDX({
   },
 });
 
-export default withMDX({
-  nextConfig,
-  pageExtensions: ['js', 'jsx', 'tsx', 'ts', 'md', 'mdx'],
-});
+export default withMDX(nextConfig);
